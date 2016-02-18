15ae5564060803990e6188e31f728c3016d09eed
K3ikoku <wall.gustaf@gmail.com>
2016-02-18 17:42:47 +0100

    Added all the prefabs of hud and car and fixed the settings to each level

--- BlitzMania/Assets/Scripts/Managers/OutOfMapTrigger.cs
+++ BlitzMania/Assets/Scripts/Managers/OutOfMapTrigger.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class OutOfMapTrigger : MonoBehaviour {
+public class OutOfMapTrigger : MonoBehaviour
+{
     
 	void OnTriggerEnter(Collider other)
     {
