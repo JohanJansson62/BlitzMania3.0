6bae4dfa1fe71fea619c26037bb86adfa93f7838
glaenn <hallda@3se-spel-17.utb.fuv.se>
2016-02-18 13:24:24 +0100

    Started HUD init and MENU

--- BlitzMania/Assets/Scripts/Managers/Menu.cs
+++ BlitzMania/Assets/Scripts/Managers/Menu.cs
@@ -1,23 +1,26 @@
-﻿//using UnityEngine;
-//using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
 
-//public class Menu : MonoBehaviour {
-//    public void GoToPage(string sceneName)
-//    {
-//        //SceneManager.LoadScene(sceneName);
-//    }
-//    public void ExitProgram()
-//    {
-//        //Application.Quit();
-//    }
-//	// Use this for initializaton
-//	void Start () {
+public class Menu : MonoBehaviour
+{
+    public void GoToPage(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    public void ExitProgram()
+    {
+        Application.Quit();
+    }
+     void Start()
+    {
 
-//	}
+    }
 
-//	// Update is called once per frame
-//	void Update () {
+    
+    void Update()
+    {
 
-//	}
-//}
+    }
+}
