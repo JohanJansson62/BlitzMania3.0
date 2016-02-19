5ac0322734bdbaa71cb4f495006f013442766fbd
glaenn <hallda@3se-spel-17.utb.fuv.se>
2016-02-19 10:00:24 +0100

    Added BobOmb varaible in Clock script

--- BlitzMania/Assets/Scripts/Managers/Clock.cs
+++ BlitzMania/Assets/Scripts/Managers/Clock.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 
 public class Clock : MonoBehaviour
 {
+
+    [SerializeField] GameObject m_bobOmb;
     private float m_timer;
     private Text m_displayTime;
 
@@ -27,7 +29,10 @@ public class Clock : MonoBehaviour
         m_timer -= Time.deltaTime; ;//removes the time since last update
         m_displayTime.text = "Time Left = " + m_timer.ToString().Substring(0,3) + "\n \n";//the \n means it's a new line
         
-
+        if(m_timer <= 3)
+        {
+            m_bobOmb.SetActive(true);
+        }
     }
 
     
